3bf4691d999f42c9c8d3aaf229d0d1971bb279d1
philippe.bloom <philippe.bloom@produweb.be>
2017-07-25 11:16:31 +0200

    webpack refactoring

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var styleLintPlugin = require('stylelint-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: [__dirname + '/src/js/app.js', __dirname + '/src/scss/app.scss'],
+    output: {
+        filename: 'dist/main.js'
+    },
+    module: {
+        rules: [
+            { // ci-dessous, on défini les regle de la tache scss
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
+        ]
+    },
+    plugins: [
+        // ci-dessous, on défini l'endroit et le nom du fichier compilé
+        new ExtractTextPlugin({
+            filename: 'dist/main.css',
+            allChunks: true,
+        }),
+    ]
+};
