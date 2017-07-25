4b5184fa318d3db2e0304eb6b0d9c67cb9a22c1f
philippe.bloom <philippe.bloom@produweb.be>
2017-07-25 13:36:28 +0200

    webpack refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
 require('jquery');
 require('tether');
 require('bootstrap');
+
 console.log('hohoho');
+(function($){
+    $( document ).ready(function() {
+        $('[data-toggle="popover"]').popover();
+        $('[data-toggle="tooltip"]').tooltip();
+        $( 'button.bd-toggle-animated-progress' ).click(function() {
+            $( this ).prev().children( '.progress-bar' ).toggleClass( 'progress-bar-animated' );
+        });
+        $('.display-4').css('color', 'blue')
+    });
+})(jQuery);
