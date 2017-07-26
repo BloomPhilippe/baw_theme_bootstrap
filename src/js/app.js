2a34dbe61311b6579ef5ff8293c9f6b4157c0465
philippe.bloom <philippe.bloom@produweb.be>
2017-07-26 15:18:18 +0200

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 console.log('hohoho');
 
-$( document ).ready(function() {
-    $('[data-toggle="popover"]').popover();
-    $('[data-toggle="tooltip"]').tooltip();
-    $( 'button.bd-toggle-animated-progress' ).click(function() {
-        $( this ).prev().children( '.progress-bar' ).toggleClass( 'progress-bar-animated' );
-    });
-    $('.display-4').css('color', 'blue')
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('button.bd-toggle-animated-progress').click(function () {
+    $(this).prev().children('.progress-bar').toggleClass('progress-bar-animated');
+  });
+  $('.display-4').css('color', 'blue')
 });
